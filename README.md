16005a567f4483aeca97ef3416bbdfcf7d03e8c9
Warren R Bank <warren.r.bank@gmail.com>
2022-01-02 14:58:41 -0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+### [Streamlare](https://github.com/warren-bank/crx-Streamlare/tree/webmonkey-userscript/es5)
+
+[Userscript](https://github.com/warren-bank/crx-Streamlare/raw/webmonkey-userscript/es5/webmonkey-userscript/Streamlare.user.js) for [streamlare.com](https://streamlare.com/) to run in both:
+* the [WebMonkey](https://github.com/warren-bank/Android-WebMonkey) application for Android
+* the [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) web browser extension for Chrome/Chromium
+
+Its purpose is to:
+* redirect embedded videos to an external player
+
+#### Legal:
+
+* copyright: [Warren Bank](https://github.com/warren-bank)
+* license: [GPL-2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
---
+++ webmonkey-userscript/Streamlare.user.js
@@ -0,0 +1,301 @@
+// ==UserScript==
+// @name         Streamlare
+// @description  Watch videos in external player.
+// @version      1.0.0
+// @match        *://streamlare.com/*
+// @match        *://*.streamlare.com/*
+// @icon         https://streamlare.com/favicon.ico
+// @run-at       document-end
+// @grant        unsafeWindow
+// @homepage     https://github.com/warren-bank/crx-Streamlare/tree/webmonkey-userscript/es5
+// @supportURL   https://github.com/warren-bank/crx-Streamlare/issues
+// @downloadURL  https://github.com/warren-bank/crx-Streamlare/raw/webmonkey-userscript/es5/webmonkey-userscript/Streamlare.user.js
+// @updateURL    https://github.com/warren-bank/crx-Streamlare/raw/webmonkey-userscript/es5/webmonkey-userscript/Streamlare.user.js
+// @namespace    warren-bank
+// @author       Warren Bank
+// @copyright    Warren Bank
+// ==/UserScript==
+
+// ----------------------------------------------------------------------------- constants
+
+var user_options = {
+  "common": {
+    "redirect_embedded_iframes":    true
+  },
+  "webmonkey": {
+    "post_intent_redirect_to_url":  "about:blank"
+  },
+  "greasemonkey": {
+    "redirect_to_webcast_reloaded": true,
+    "force_http":                   true,
+    "force_https":                  false
+  }
+}
+
+// ----------------------------------------------------------------------------- helpers (xhr)
+
+var serialize_xhr_body_object = function(data) {
+  if (typeof data === 'string')
+    return data
+
+  if (!(data instanceof Object))
+    return null
+
+  var body = []
+  var keys = Object.keys(data)
+  var key, val
+  for (var i=0; i < keys.length; i++) {
+    key = keys[i]
+    val = data[key]
+    val = unsafeWindow.encodeURIComponent(val)
+
+    body.push(key + '=' + val)
+  }
+  body = body.join('&')
+  return body
+}
+
+var download_text = function(url, headers, data, callback) {
+  if (data) {
+    if (!headers)
+      headers = {}
+    if (!headers['content-type'])
+      headers['content-type'] = 'application/x-www-form-urlencoded'
+
+    switch(headers['content-type'].toLowerCase()) {
+      case 'application/json':
+        data = JSON.stringify(data)
+        break
+
+      case 'application/x-www-form-urlencoded':
+      default:
+        data = serialize_xhr_body_object(data)
+        break
+    }
+  }
+
+  var xhr    = new unsafeWindow.XMLHttpRequest()
+  var method = data ? 'POST' : 'GET'
+
+  xhr.open(method, url, true, null, null)
+
+  if (headers && (typeof headers === 'object')) {
+    var keys = Object.keys(headers)
+    var key, val
+    for (var i=0; i < keys.length; i++) {
+      key = keys[i]
+      val = headers[key]
+      xhr.setRequestHeader(key, val)
+    }
+  }
+
+  xhr.onload = function(e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(xhr.responseText)
+      }
+    }
+  }
+
+  if (data)
+    xhr.send(data)
+  else
+    xhr.send()
+}
+
+var download_json = function(url, headers, data, callback) {
+  if (!headers)
+    headers = {}
+  if (!headers.accept)
+    headers.accept = 'application/json'
+
+  download_text(url, headers, data, function(text){
+    try {
+      callback(JSON.parse(text))
+    }
+    catch(e) {}
+  })
+}
+
+// ----------------------------------------------------------------------------- URL links to tools on Webcast Reloaded website
+
+var get_webcast_reloaded_url = function(video_url, vtt_url, referer_url, force_http, force_https) {
+  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.greasemonkey.force_http
+  force_https = (typeof force_https === 'boolean') ? force_https : user_options.greasemonkey.force_https
+
+  var encoded_video_url, encoded_vtt_url, encoded_referer_url, webcast_reloaded_base, webcast_reloaded_url
+
+  encoded_video_url     = encodeURIComponent(encodeURIComponent(btoa(video_url)))
+  encoded_vtt_url       = vtt_url ? encodeURIComponent(encodeURIComponent(btoa(vtt_url))) : null
+  referer_url           = referer_url ? referer_url : unsafeWindow.location.href
+  encoded_referer_url   = encodeURIComponent(encodeURIComponent(btoa(referer_url)))
+
+  webcast_reloaded_base = {
+    "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html",
+    "http":  "http://webcast-reloaded.surge.sh/index.html"
+  }
+
+  webcast_reloaded_base = (force_http)
+                            ? webcast_reloaded_base.http
+                            : (force_https)
+                               ? webcast_reloaded_base.https
+                               : (video_url.toLowerCase().indexOf('http:') === 0)
+                                  ? webcast_reloaded_base.http
+                                  : webcast_reloaded_base.https
+
+  webcast_reloaded_url  = webcast_reloaded_base + '#/watch/' + encoded_video_url + (encoded_vtt_url ? ('/subtitle/' + encoded_vtt_url) : '') + '/referer/' + encoded_referer_url
+  return webcast_reloaded_url
+}
+
+// ----------------------------------------------------------------------------- URL redirect
+
+var redirect_to_url = function(url) {
+  if (!url) return
+
+  if (typeof GM_loadUrl === 'function') {
+    if (typeof GM_resolveUrl === 'function')
+      url = GM_resolveUrl(url, unsafeWindow.location.href) || url
+
+    GM_loadUrl(url, 'Referer', unsafeWindow.location.href)
+  }
+  else {
+    try {
+      unsafeWindow.top.location = url
+    }
+    catch(e) {
+      unsafeWindow.window.location = url
+    }
+  }
+}
+
+var process_webmonkey_post_intent_redirect_to_url = function() {
+  var url = null
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'string')
+    url = user_options.webmonkey.post_intent_redirect_to_url
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'function')
+    url = user_options.webmonkey.post_intent_redirect_to_url()
+
+  if (typeof url === 'string')
+    redirect_to_url(url)
+}
+
+var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
+  if (!video_url)
+    return
+
+  if (!referer_url)
+    referer_url = unsafeWindow.location.href
+
+  if (typeof GM_startIntent === 'function') {
+    // running in Android-WebMonkey: open Intent chooser
+
+    var args = [
+      /* action = */ 'android.intent.action.VIEW',
+      /* data   = */ video_url,
+      /* type   = */ video_type
+    ]
+
+    // extras:
+    if (vtt_url) {
+      args.push('textUrl')
+      args.push(vtt_url)
+    }
+    if (referer_url) {
+      args.push('referUrl')
+      args.push(referer_url)
+    }
+
+    GM_startIntent.apply(this, args)
+    process_webmonkey_post_intent_redirect_to_url()
+    return true
+  }
+  else if (user_options.greasemonkey.redirect_to_webcast_reloaded) {
+    // running in standard web browser: redirect URL to top-level tool on Webcast Reloaded website
+
+    redirect_to_url(get_webcast_reloaded_url(video_url, vtt_url, referer_url))
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+var process_hls_url = function(hls_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ hls_url, /* video_type= */ 'application/x-mpegurl', vtt_url, referer_url)
+}
+
+var process_dash_url = function(dash_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ dash_url, /* video_type= */ 'application/dash+xml', vtt_url, referer_url)
+}
+
+var process_mp4_url = function(mp4_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ mp4_url, /* video_type= */ 'video/mp4', vtt_url, referer_url)
+}
+
+// ----------------------------------------------------------------------------- process page
+
+var resolve_url = function(url) {
+  if (url.substring(0, 4).toLowerCase() === 'http')
+    return url
+
+  if (url.substring(0, 2) === '//')
+    return unsafeWindow.location.protocol + url
+
+  if (url.substring(0, 1) === '/')
+    return unsafeWindow.location.protocol + '//' + unsafeWindow.location.host + url
+
+  return unsafeWindow.location.protocol + '//' + unsafeWindow.location.host + unsafeWindow.location.pathname.replace(/[^\/]+$/, '') + url
+}
+
+var process_video_page = function() {
+  var video_id, xhr_url, xhr_data, callback
+
+  regex = {
+    pathname: new RegExp('/e/([^/]+)(?:/.*)?$')
+  }
+
+  if (!regex.pathname.test(unsafeWindow.location.pathname))
+    return
+
+  video_id = unsafeWindow.location.pathname.replace(regex.pathname, '$1')
+  xhr_url  = 'https://streamlare.com/api/video/get'
+  xhr_data = {id: video_id}
+  callback = function(data) {
+    var mp4_url
+
+    if (data && (data instanceof Object) && (data.status === 'success') && (data.result instanceof Object) && (data.result.Original instanceof Object) && data.result.Original.src) {
+      mp4_url  = data.result.Original.src
+      mp4_url  = resolve_url(mp4_url)
+      mp4_url += '#video.mp4'
+
+      process_mp4_url(mp4_url)
+    }
+  }
+
+  download_json(xhr_url, /* headers= */ null, xhr_data, callback)
+}
+
+// ----------------------------------------------------------------------------- bootstrap
+
+var is_video_page = (unsafeWindow.location.pathname.indexOf('/e/') === 0)
+
+var should_process_window = function() {
+  var is_top = (unsafeWindow.window === unsafeWindow.top)
+
+  return is_top || user_options.common.redirect_embedded_iframes
+}
+
+var should_init = function() {
+  return is_video_page && should_process_window()
+}
+
+var init = function() {
+  process_video_page()
+}
+
+if (should_init())
+  init()
+
+// -----------------------------------------------------------------------------
